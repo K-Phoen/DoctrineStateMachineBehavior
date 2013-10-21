3b80ef47abb91df70bd12b30fe39ac52571cd638
Kévin Gomez <geek63@gmail.com>
2013-10-21 08:49:28 +0100

    Added a few comments for the Stateful interface and the StatefulTrait

--- src/KPhoen/DoctrineStateMachineBehavior/Entity/Stateful.php
+++ src/KPhoen/DoctrineStateMachineBehavior/Entity/Stateful.php
@@ -6,11 +6,25 @@ use Finite\StateMachine\StateMachine;
 use Finite\StatefulInterface;
 
 /**
+ * Interface describing the methods expected in stateful entities.
+ *
+ * @see StatefulTrait
+ *
  * @author Kévin Gomez <contact@kevingomez.fr>
  */
 interface Stateful extends StatefulInterface
 {
+    /**
+     * Set the state machine to be used by the entity.
+     *
+     * @param StateMachine $stateMachine
+     */
     public function setStateMachine(StateMachine $stateMachine);
 
+    /**
+     * Get the state machine used by the entity.
+     *
+     * @return StateMachine
+     */
     public function getStateMachine();
 }
--- src/KPhoen/DoctrineStateMachineBehavior/Entity/StatefulTrait.php
+++ src/KPhoen/DoctrineStateMachineBehavior/Entity/StatefulTrait.php
@@ -5,22 +5,48 @@ namespace KPhoen\DoctrineStateMachineBehavior\Entity;
 use Finite\StateMachine\StateMachine;
 
 /**
+ * Trait implementing useful methods for stateful entities.
+ *
+ * Expose magic methods based on the transition allowed by te state-machine:
+ *  * {TransitionName}(): apply the transition {TransitionName} ;
+ *  * can{TransitionName}(): test if the transition {TransitionName} can be applied.
+ *
  * @author Kévin Gomez <contact@kevingomez.fr>
  */
 trait StatefulTrait
 {
+    /**
+     * @var StateMachine
+     */
     protected $stateMachine;
 
+    /**
+     * Set the state machine to be used by the entity.
+     *
+     * @param StateMachine $stateMachine
+     */
     public function setStateMachine(StateMachine $stateMachine)
     {
         $this->stateMachine = $stateMachine;
     }
 
+    /**
+     * Get the state machine used by the entity.
+     *
+     * @return StateMachine
+     */
     public function getStateMachine()
     {
         return $this->stateMachine;
     }
 
+    /**
+     * Test if the given transition can be applied.
+     *
+     * @param string $transition The transition name.
+     *
+     * @return bool
+     */
     public function can($transition)
     {
         return $this->stateMachine->can($transition);
@@ -32,7 +58,7 @@ trait StatefulTrait
 
         if (substr($method, 0, 3) === 'can' && isset($transitions[strtolower(substr($method, 3))])) {
             return $this->stateMachine->can(strtolower(substr($method, 3)));
-        } else if (isset($transitions[$method])) {
+        } elseif (isset($transitions[$method])) {
             $this->stateMachine->apply($method);
         }
     }
